d17ae576d53eeab9ed6c0fba9bcb1b27d2549263
Felipe Marcelino Andre <f.marcelino.a@gmail.com>
2021-02-27 15:48:09 -0300

    Adicionando soma.js

---
+++ app.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+let moment = require('moment');
+
+
+const superHerois = require("./superHerois");
+
+let dados = fs.readFileSync(__dirname + '/dados.txt', 'utf8');
+let data = moment().format('MMMM Do YYYY, h:mm:ss a');
+
+console.log(superHerois + "oi");
+
+console.log ("Salvando no git");
